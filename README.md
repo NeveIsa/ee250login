99d103588c15467dbb178e4a95a3e4fe017ec59d
sampad <sbmohant@usc.edu>
2021-01-13 19:32:22 -0800

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ee250login
+
+- @2021-01-14T03:31:26.373546	sbmohant@usc.edu
--- landing.py
+++ landing.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
-import os
+import os,sys,datetime,time,signal
+logfile = 'README.md'
 
 #https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
 class bcolors:
@@ -14,7 +15,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-welcome=f"""
+intro=f"""
 {bcolors.BOLD}
 	__________________________________________________
 	EE250 - Distributed Systems and Internet Of Things
@@ -32,9 +33,77 @@ welcome=f"""
 	4. Your logins to this RPi are timestamped and recorded.
 
 """
-print(welcome)
+
+outro=f"""
+{bcolors.WARNING+bcolors.BOLD}
+	__________________________________
+	THANK YOU AND HAPPY LEARNING EE250
+	``````````````````````````````````
+"""
+
+failedloginmsg=f"""
+{bcolors.WARNING+bcolors.BOLD} Login Failed! This event has been recored.
+"""
+
+def logEvent(text):
+    with open(logfile,'a+') as g:
+        g.write(text)
+
+def login():
+    
+    os.system('git pull')
+    
+    print(f'{bcolors.BOLD+bcolors.OKBLUE}We use Github [using `git push`] for login and USC Email for recording login events')
+    while True:
+        print(f"{bcolors.WARNING+bcolors.BOLD}Email (@usc.edu): ",end=f'{bcolors.ENDC}')
+        uscemail = input().strip()
+        if uscemail=='' or (not uscemail.endswith('@usc.edu')):
+            continue
+
+        else: break
+
+    logtime = datetime.datetime.utcnow().isoformat()
+    
+    logtext = f"\n- @{logtime}\t{uscemail}"
+    
+    logEvent(logtext)
+
+    os.system('git add log.txt')
+    os.system('git commit -m "{logtext}"')
+    cmdstatus = os.system('git push')
+    
+    if(cmdstatus==0):
+        print(intro)
+        os.system('bash')
+        print(outro)
+        time.sleep(3)
+        sys.exit(0)
+    else:
+        logEvent('(FAILED)')
+        print(failedloginmsg)
+        time.sleep(3)
+        sys.exit(-1)
+
+def signal_handler(sig, frame):
+    logtime = datetime.datetime.utcnow().isoformat()
+    logEvent(f"\n- @{logtime}\t*SIGINT*")
+    print(f"\n\n{bcolors.WARNING+bcolors.BOLD}INTERRUPT -> This event has been logged.")
+    time.sleep(3)
+    sys.exit(-2)
 
 
+#change to current dir
+thisfile = os.path.realpath(__file__)
+thisdir = os.path.dirname(thisfile)
+os.chdir(thisdir)
+
+#register signal interrupt handler
+signal.signal(signal.SIGINT, signal_handler)
+
+#attempt login
+login()
+
+#exit
+sys.exit(-3) #shouldn't reach here, must be captured by SIGINT
 
 
-os.system('bash')
